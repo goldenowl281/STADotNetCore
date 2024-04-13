bca7cf9d93ae5ab4c9dbc66e2766528d77c83cee
GoldenOwl <rathart281@gmail.com>
2024-04-13 17:21:32 +0630

    first commit

--- STADotNetCore.ConsoleApp/obj/Debug/net8.0/STADotNetCore.ConsoleApp.AssemblyInfo.cs
+++ STADotNetCore.ConsoleApp/obj/Debug/net8.0/STADotNetCore.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("STADotNetCore.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3a874fda6794eed4abbd0e6360c44c54d9b43ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("STADotNetCore.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("STADotNetCore.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
