2a1b97ef6aabc1643286915910d7fb47fea77538
GoldenOwl <rathart281@gmail.com>
2024-04-14 12:34:16 +0630

    Day 1 Ado.Net Read processing to CRUD

--- STADotNetCore.ConsoleApp/Program.cs
+++ STADotNetCore.ConsoleApp/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+
+stringBuilder.DataSource        = ".";
+stringBuilder.InitialCatalog    = "DotNetTrainingBatch4";
+stringBuilder.UserID            = "sa";
+stringBuilder.Password          = "sasa@123";
+
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+
+connection.Open();
+Console.WriteLine("connection open");
+
+string query = "SELECT * FROM Tbl_Blog";
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+sqlDataAdapter.Fill(dt);
+
+connection.Close();
+Console.WriteLine("connection close ");
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine("Blog Id      => " + dr["BlogId"]);
+    Console.WriteLine("Blog Title   => " + dr["BlogTitle"]);
+    Console.WriteLine("Blog Author  => " + dr["BlogAuthor"]);
+    Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+    Console.WriteLine("-------*******************--------");
+}
+Console.ReadKey();
--- STADotNetCore.ConsoleApp/obj/Debug/net8.0/STADotNetCore.ConsoleApp.AssemblyInfo.cs
+++ STADotNetCore.ConsoleApp/obj/Debug/net8.0/STADotNetCore.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("STADotNetCore.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca7cf9d93ae5ab4c9dbc66e2766528d77c83cee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4523ae025742fc458a87a4c10353881210ed69c")]
 [assembly: System.Reflection.AssemblyProductAttribute("STADotNetCore.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("STADotNetCore.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
